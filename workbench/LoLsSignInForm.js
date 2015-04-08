632b3986dc9defed7379279ee6abe76d06996c27
Jamie Douglass <jamie@douglass.com>
2015-04-07 19:32:22 -0700

    MVP with bug fixes

--- workbench/LoLsSignInForm.js
+++ workbench/LoLsSignInForm.js
@@ -1,15 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-<title>Sign in Language of Languages workbench</title>
-<meta charset="UTF-8">
-</head>
-<body>
-<h1>Language of Languages workbench federated sign in</h1>
-<p>Work in Progress! Coming 2015 
-<br><br>
-<script src="hello.js"></script>
+function setupSignInForm(elem) {
+	var str='<div id="SignInWorkbench">' +
+	'<div>' +
+	'	<form autocomplete="off">' +
+	'		<h3>Sign In Workspace</h3>' +
+	'		<button class ="zocial icon google" title = "Sign into Google" onclick="hello(\'google\').login()">Google Sign-in</button>' +
+	'		<button type="button" title="cancel" onClick="hideForm(\'SignInWorkbench\')">' +
+	'			Cancel' +
+	'		</button>' +
+	'	</form>' +
+	'</div>' +
+  '</div>';
+	elem.insertAdjacentHTML("beforeend", str);	
+}
+function signInForm() {
+  showForm('SignInWorkbench');
+}
+/*
+<!--convert html to str then insert like above
 <link rel="stylesheet" href="css-social-buttons/css/zocial.css" />
 <button class ="zocial icon google" title = "Sign into Google" onclick="hello('google').login()">Google Sign-in</button>
 <button class ="zocial icon github" title = "Sign into GitHub" onclick="hello('github').login()">GitHub Sign-in</button>
@@ -17,12 +24,13 @@
 <button class ="zocial icon facebook" title = "Sign into Facebook" onclick="hello('facebook').login()">Facebook Sign-in</button>
 <button class ="zocial icon yahoo" title = "Sign into Yahoo" onclick="hello('yahoo').login()">Yahoo Sign-in</button>
 <button title = "Log out of Google" onclick="hello('google').logout()">Google Sign-out</button>
-<script>
+-->
+*/
+
 hello.init({
 	google : '137204177451-16si0pna0ohluv8eurf44qgrfel09dq1.apps.googleusercontent.com'
 },{redirect_uri:'http://www.languageoflanguages.com/workbench/index.html'});
-</script>
-<script>
+
 hello.on('auth.login', function(auth) {
 	hello(auth.network).api('/me').then(function(r) {
 		var label = document.getElementById("profile_"+auth.network);
@@ -34,16 +42,3 @@ hello.on('auth.login', function(auth) {
 		label.innerHTML = '<img src ="'+r.thumbnail+'"/>Hey'+r.name;
 	});
 });
-</script>
-<!-- 
-<script>
-hello("google").logout().then(function(){
-alert("Signed out");
-}, function(e){
-alert("Signed out error:" +e.error.message);
-});
-</script>
- -->
-
-</body>
-</html>
